ebff4d486dbe104335650d90f7f0d0d6f4946db3
manu2345-Cpu <manu111dwivedi@gmail.com>
2025-01-23 11:42:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,8 +15,5 @@ DB_URI = os.environ.get("DB_URI", "")
 #Your Logs Channel/Group ID
 LOGS_CHAT_ID = int(os.environ.get("LOGS_CHAT_ID", ""))
 
-#Force Sub Channel ID
-FSUB_ID = int(os.environ.get("FSUB_ID", ""))
-
 #Force Sub Channel Invite Link
 FSUB_INV_LINK = os.environ.get("FSUB_INV_LINK", "")
