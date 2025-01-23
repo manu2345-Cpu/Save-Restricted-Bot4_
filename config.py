d89017808b570a449bcd0e3a37fca06d130104d7
manu2345-Cpu <manu111dwivedi@gmail.com>
2025-01-23 11:37:59 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -8,3 +8,15 @@ API_ID = int(os.environ.get("API_ID", ""))
 
 #Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
+
+#Database 
+DB_URI = os.environ.get("DB_URI", "")
+
+#Your Logs Channel/Group ID
+LOGS_CHAT_ID = int(os.environ.get("LOGS_CHAT_ID", ""))
+
+#Force Sub Channel ID
+FSUB_ID = int(os.environ.get("FSUB_ID", ""))
+
+#Force Sub Channel Invite Link
+FSUB_INV_LINK = os.environ.get("FSUB_INV_LINK", "")
